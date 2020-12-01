9ee07484903d2799378b750510cac115c0ec14b2
Tim Nash <site@timnash.co.uk>
2020-12-01 19:49:32 +0000

    Updated to add a hash example

--- tn-security-headers-examples.php
+++ tn-security-headers-examples.php
@@ -69,3 +69,36 @@ function tn_filter_content_security_policy( $csp ){
     return $csp;
 }
 add_filter( 'tn_content_security_policy', 'tn_filter_content_security_policy', 1 );
+
+ /*
+  * Example of using sha256 hash on an inline script
+  * Please don't use it, its a terrible example
+  * 
+  */
+class hashExample{
+    //Our script and holder for hash
+    private     $sha256;
+    private     $script = '<script>
+                        alert("Hello! I am an alert box!");
+                        </script>';
+
+    public function __construct(){
+        // Hash the script
+        $this->sha256 = 'sha256-'.hash( 'sha256', $this->script );
+        // Add the hash to sources
+        add_filter( 'tn_content_security_policy_sources', array( $this, 'content_security_policy_sources' ), 1 );
+        // Add the hash to the policy
+        add_filter( 'tn_content_security_policy', array( $this, 'content_security_policy' ), 1 );
+    }
+
+    public function content_security_policy_sources( $sources ){
+        $sources[] = $this->sha256;
+        return $sources;
+    }
+
+    public function content_security_policy( $csp ){
+        $csp['fetch']['script-src'][] = $this->sha256;
+        return $csp;
+    }
+}
+new hashExample();
